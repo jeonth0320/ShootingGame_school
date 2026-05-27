ebff84b1621fbf8d16a7db415c5c7eb45afc09c7
jeonth0320 <jeonth0320@naver.com>
2026-05-27 14:40:57 +0900

    fdsf

--- Assets/Project/Scripts/Inventory/InventoryUI.cs
+++ Assets/Project/Scripts/Inventory/InventoryUI.cs
@@ -12,13 +12,28 @@ public class InventoryUI : MonoBehaviour
     }
 
     public void Toggle()
-    {
+    { 
+        if (inventoryPanel= null)
+        {
+            Debug.LogWarning("Inventory Panel이 연결되지 않았습니다");
+
+            return;
+        }
+
+        bool nextOpen = !inventoryPanel.activeSelf;
+
+        inventoryPanel.SetActive(!inventoryPanel.activeSelf);
         // 패널 열기/닫기
         // 열릴 때 Refresh() 호출
     }
 
     public void Refresh()
     {
+        PlayerInventory inventory = PlayerInventory.Instance;
+
+        if(inventory == null) return;
+
+        for 
         // bagSlots와 equipSlots에 PlayerInventory의 리스트 연결
     }
 }
